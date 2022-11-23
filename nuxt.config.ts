72a0b9c46839819f7222dec387f4b26ce15713c5
Arturas Drumsta <drumsta@users.noreply.github.com>
2022-11-23 01:27:18 +0000

    update

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    imports: {
+        autoImport: false
+      },
+      typescript: {
+        strict: true
+      }
+})
