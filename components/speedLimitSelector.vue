bd0896e7477025f217fd472c907f73895679a979
Arturas Drumsta <drumsta@users.noreply.github.com>
2022-12-17 13:46:15 +0000

    update

--- components/speedLimitSelector.vue
+++ components/speedLimitSelector.vue
@@ -4,6 +4,15 @@
 
 <template>
   <div>
-    <q-knob v-model="speedLimit" class="q-ma-md" :step="10" show-value size="50px" color="lime" :min="0" :max="100" />
+    <q-knob
+      v-model="speedLimit"
+      class="text-orange-600 q-ma-md"
+      :step="10"
+      show-value
+      size="90px"
+      :thickness="0.22"
+      color="text-orange"
+      track-color="text-orange-500"
+    />
   </div>
 </template>
